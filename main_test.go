f21cec3038f70a38a3a1a3e97349febfdffb34b1
cormachogan <chogan@vmware.com>
2022-01-07 10:33:45 +0000

    add test

--- main_test.go
+++ main_test.go
@@ -13,6 +13,18 @@ import (
 
 type Server struct{}
 
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+   w.WriteHeader(http.StatusOK)
+   w.Header().Set("Content-Type", "application/json")
+   w.Write([]byte(`{"message": "Hello World!!!"}`))
+}
+
+func main() {
+   s := &Server{}
+   http.Handle("/", s)
+   log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
 func TestServeHTTP(t *testing.T) {
         s := &Server{}
   	r, _ := http.NewRequest("GET", "/", nil)
