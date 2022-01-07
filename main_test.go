e1fadf65f3b83c4c72e00367c95ebfae016bd684
cormachogan <chogan@vmware.com>
2022-01-07 10:18:07 +0000

    added valid test

--- main_test.go
+++ main_test.go
@@ -7,9 +7,16 @@ package main
 import (
    "log"
    "net/http"
+   "net/http/httptest"
+   "testing"
+   "strings"
 )
 
 type Server struct{}
+
+// statusHandler is an http.Handler that writes an empty response using itself
+// as the response status code.
+type statusHandler int
 
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
    w.WriteHeader(http.StatusOK)
@@ -21,4 +28,14 @@ func main() {
    s := &Server{}
    http.Handle("/", s)
    log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func TestServeHTTP(t *testing.T) {
+        s := &Server{}
+  	r, _ := http.NewRequest("GET", "/", nil)
+	w := httptest.NewRecorder()
+	s.ServeHTTP(w, r)
+	if b := w.Body.String(); !strings.Contains(b, "hello world") {
+		t.Fatalf("body = %s, want no", b)
+	}
 }
